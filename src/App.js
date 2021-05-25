ff163edfebf97d304f3ac3df37d079a1d613c035
Sara Iftikhar <siftikhar@ssidecisions.com>
2021-05-26 02:46:29 +0500

    Login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
+import LoginForm from "./components/loginForm";
 import "./App.css";
-import Movies from './components/movies'
 
 function App() {
-  return <main className = "container"> 
-  <Movies></Movies>
-   </main>;
+  return (
+    <React.Fragment>
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+        </Switch>
+      </main>
+      ;
+    </React.Fragment>
+  );
 }
 
 export default App;
